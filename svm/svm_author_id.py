195e42756a76109e03ac7b6146f5b4e81ad34372
simplegr33n <gcgolda@gmail.com>
2017-07-08 21:00:23 -0700

    SVM mini proj

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,6 +25,39 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn import svm
+clf = svm.SVC(kernel='rbf',C=10000.0,gamma='auto')
+
+# Reduce to 1% of data to speed up svm training and prediction
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
+
+
+# Train and time the train
+t0 = time()
+print "training..."
+clf.fit(features_train, labels_train)
+print "train time:", round(time()-t0, 3), "s"
+
+# Predict and time prediction
+t0 = time()
+print "predicting..."
+pred = clf.predict(features_test)
+print "predict time:", round(time()-t0, 3), "s"
+
+# Prediction at 10, 26, 50
+print "10th: %r, 26th: %r, 50th: %r" % (pred[10], pred[26], pred[50])
+
+# Number predicted to be Chris(1)
+print "No. of predicted to be in the 'Chris'(1): %r" % sum(pred) #simply find sum, as Sara is (0)
+
+
+
+from sklearn.metrics import accuracy_score
+print "accuracy score: ", accuracy_score(labels_test, pred)
+
+
+
 #########################################################
 
 
